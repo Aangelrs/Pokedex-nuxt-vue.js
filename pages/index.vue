000d928663c4b0aa9ea549238fccda28acd76fa1
Aangelrs <angelrangel561@gmail.com>
2022-09-06 10:28:49 -0400

    AddSetion Pokedex- Title barUp

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,8 @@
 <template>
+  <h1>Pokedex</h1>
+</template>
+
+<template>
   <v-row justify="center" align="center">
     <v-col cols="12" sm="8" md="6">
       <v-card class="logo py-4 d-flex justify-center">
